f58b6c48cd4f06994d65f5edd0bd749e20e5bda2
HowardKiat <buisnesskiat123@gmail.com>
2024-09-28 18:52:36 +0800

    Review (5% completion)

---
+++ DataStructureG43/Review.hpp
@@ -0,0 +1,18 @@
+//#pragma once
+//
+//#ifndef REVIEW_HPP
+//#define REVIEW_HPP
+//
+//#include <string>
+//
+//class Review
+//{
+//private:
+//	string feedback;
+//	int rating;
+//
+//public:
+//	Review();
+//
+//
+//};
