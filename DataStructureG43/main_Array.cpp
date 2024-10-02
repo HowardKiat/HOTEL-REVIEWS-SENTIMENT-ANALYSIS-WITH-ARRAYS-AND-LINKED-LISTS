4c8e112e90798907b6b281cc12418443a7018508
HowardKiat <buisnesskiat123@gmail.com>
2024-10-03 00:44:00 +0800

    Name Change (might change again)

---
+++ DataStructureG43/main_Array.cpp
@@ -0,0 +1,32 @@
+#include "sentiment_analysis.hpp"
+#include <iostream>
+
+int main()
+{
+    // Create an instance of the SentimentAnalysis class on the heap
+    SentimentAnalysis* analysis = new SentimentAnalysis();
+
+    // Load positive and negative words from files
+    analysis->loadPositiveWords();
+    analysis->loadNegativeWords();
+
+    //Load Reviews from CSV files
+    analysis->loadCSV("tripadvisor_hotel_reviews.csv");
+
+    // Manually add some reviews for testing purposes (FOR TESTING USE - HOWARD :))
+    analysis->addReview("This hotel is amazing and the service is great!", 5);
+    analysis->addReview("The food was terrible, and the noise was unbearable.", 2);
+    analysis->addReview("The location is perfect, highly recommended.", 5);
+    analysis->addReview("Not worth the money.", 1);
+
+    // Perform sentiment analysis on the reviews
+    analysis->analyzeSentimentReviews();
+
+    // Calculate and display overall sentiment
+    analysis->calculateOverallSentiment();
+
+    // Clean up the allocated memory
+    delete analysis;
+
+    return 0;
+}
